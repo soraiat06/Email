cc8ab36afbf8d698664fa486924d422fe94b0062
Soraia Tavares <soraiatosdeoliveira@hotmail.com>
2024-04-16 08:55:45 -0300

    Commit da última modificação da atividade de email

--- app/src/main/java/tavares/soraia/email/MainActivity.java
+++ app/src/main/java/tavares/soraia/email/MainActivity.java
@@ -2,7 +2,14 @@ package tavares.soraia.email;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnEnviar = (Button) findViewById(R.id.btnEnviar);
+        // Define ação do clique do botão
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtém dados digitados pelo usuário
+                EditText etEmail = (EditText) findViewById(R.id.etEmail);
+                String email = etEmail.getText().toString();
+
+                EditText etAssunto = (EditText) findViewById(R.id.etAssunto);
+                String assunto = etAssunto.getText().toString();
+
+                EditText etTexto = (EditText) findViewById(R.id.etTexto);
+                String texto = etTexto.getText().toString();
+
+                // Cria nova intenção para enviar e-mail
+                Intent i = new Intent(Intent.ACTION_SENDTO);
+                i.setData(Uri.parse("mailto:"));
+
+                // Define destinatário, assunto e corpo do e-mail
+                String[] emails = new String[]{email};
+                i.putExtra(Intent.EXTRA_EMAIL, emails);
+                i.putExtra(Intent.EXTRA_SUBJECT, assunto);
+                i.putExtra(Intent.EXTRA_TEXT, texto);
+
+                // Tenta iniciar a atividade para enviar e-mail
+                try {
+                    startActivity(Intent.createChooser(i, "Escolha o APP"));
+                } catch (ActivityNotFoundException e) {
+                    // Exibe mensagem se não houver aplicativos para realizar a operação
+                    Toast.makeText(MainActivity.this, "Não há nenhum app que possa realizar essa operação", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
